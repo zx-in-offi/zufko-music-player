ff7fd0cd04fbb7b2572b6cf36616c20bc8c1dd06
Varshith8868 <konalavasrshith@gmail.com>
2024-10-08 12:45:46 +0530

    change

--- Music_Player_Django/App/urls.py
+++ Music_Player_Django/App/urls.py
@@ -1,12 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 app_name = "App"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-
-
+    path("", views.landing_page, name="landing_page"),  # Route for the landing page
+    path("songs/", views.index, name="index"),          # Route for the songs page
 ]
+
+# Serve static files in development (if needed)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Music_Player_Django/App/views.py
+++ Music_Player_Django/App/views.py
@@ -1,15 +1,15 @@
-# Create your views here.
-from django.shortcuts import render,redirect
-# imported our models
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
-from . models import Song
+from .models import Song
 
+# Landing page view
+def landing_page(request):
+    return render(request, 'landing_page.html')
 
-
+# Index view for songs
 def index(request):
-    paginator= Paginator(Song.objects.all(),1)
+    paginator = Paginator(Song.objects.all(), 1)  # Change '1' to the number of songs you want per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    context={"page_obj":page_obj}
-    return render(request,"index.html",context)
-
+    context = {"page_obj": page_obj}
+    return render(request, "index.html", context)
